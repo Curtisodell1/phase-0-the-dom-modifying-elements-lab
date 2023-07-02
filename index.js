cc348659b934ca858cd1e7564460b5e31e924a38
Curtis O'Dell <curtisodell1@gmail.com>
2023-07-02 16:05:53 -0400

    editing html via JS

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const changeName = document.getElementById('main');
+changeName.id = 'Curtis O\'Dell'
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent="Curtis is the champion"
